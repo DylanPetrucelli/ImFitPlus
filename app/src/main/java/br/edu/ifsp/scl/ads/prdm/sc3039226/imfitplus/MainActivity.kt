5fcdd1479a349fb164c46bad69990b9bfa6f8f4b
DylanPetrucelli <dtpetrucelli@gmail.com>
2025-10-30 13:23:32 -0300

    feat: add intent from mainActivity to DadosActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/imfitplus/MainActivity.kt
@@ -1,20 +1,27 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039226.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039226.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.welcomeBtn.setOnClickListener {
+            val intent = Intent(this, DadosActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
