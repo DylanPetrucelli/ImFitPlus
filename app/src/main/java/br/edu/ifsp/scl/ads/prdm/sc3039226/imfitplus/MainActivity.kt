26f6f2e5aa846ab4ad81afb32ca76fdc30c55ab7
DylanPetrucelli <dtpetrucelli@gmail.com>
2025-10-30 13:24:18 -0300

    refactor: remove unused imports

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/imfitplus/MainActivity.kt
@@ -2,20 +2,17 @@ package br.edu.ifsp.scl.ads.prdm.sc3039226.imfitplus
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc3039226.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding;
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater);
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.welcomeBtn.setOnClickListener {
